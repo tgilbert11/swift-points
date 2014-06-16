0161a414ec0f256a203b1cd5dc93ce1e5e26d28c
Taylor H. Gilbert <taylorg@192.168.1.2>
2014-06-16 20:59:54 +0800

    Initial Commit

---
+++ SwiftPoints/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftPoints
+//
+//  Created by Taylor H. Gilbert on 6/16/14.
+//  Copyright (c) 2014 Taylor H. Gilbert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
